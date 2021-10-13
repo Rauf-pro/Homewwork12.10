a10dee4b2f70cb4a3a56cc7812acdc7c15ba242d
Rauf-pro <87373669+Rauf-pro@users.noreply.github.com>
2021-10-13 12:29:12 +0400

    added two task

---
+++ JS/script.js
@@ -0,0 +1,46 @@
+ //TASK- 1 getStudentName
+  /*
+  class studentsname{
+       constructor([name]){
+           this.name = [name];
+       }
+       getStudentName(){
+
+           return this.name;
+       }
+  }    
+  let student1 = "Steve";
+  let student2 = "Mike";
+  let studen3 = "John"
+
+console.log([student1,student2,studen3]);
+*/
+ 
+
+// TASK -2 cityFacts
+
+class cityFacts{
+    constructor(Name, Population, Continent){
+           this.name = Name;
+           this.population= Population;
+           this.continent = Continent;
+}
+        
+ }
+
+ let showCity = obj => {
+     if (this.name == X && this.population == Y && this.continent == Z ) {
+         return true;
+     }
+     
+ }
+ 
+ let city1 = new cityFacts ("Paris", "2,140,526", "Europa");
+ let city2 = new cityFacts ("Tokyo", "13,929,286", "Asia");
+
+ console.log (city1.name + " has a population of " + city1.population + " and is situated in " + city1.continent);
+ console.log (city2.name + " has a population of " + city2.population + " and is situated in " + city2.continent);
+ 
+
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Homewwork12.10
